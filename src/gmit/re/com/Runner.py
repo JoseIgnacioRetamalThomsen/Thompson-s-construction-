384b9db084cd77d78b65b640795ff0c6f23ae016
Jose Ignacio Retamal <peperetamalthomsen@outlook.com>
2019-03-27 23:13:42 +0000

    Thomsons module comments reviewed

--- src/gmit/re/com/Runner.py
+++ src/gmit/re/com/Runner.py
@@ -10,14 +10,16 @@ class Runner:
 
         ThomsonsMap.NFA.initStateCount(1)
 
+        #a?nan
+        #a?^n.a^n a^n a?.a?.a?.a.a.a a.a.a
         string = "/*.((a-z)|(0-9)|/.)*.@.((a-z)|(0-9))*./..(a-z)+"
         #s1 = "(0|(1.(0.1*(0.0)*.0)*.1)*)*"
-        s1 = "a?"
+        s1 = "a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a"
         p = Shunting.Converter().toPofix(s1)
         print(p)
         n = ThomsonsMap.compile(p)
        # n.tf.printF()
-        print(n.run("aa"))
+        print(n.run("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"))
 
         n = ThomsonsMap.NFA("z")
         n.plus()
@@ -25,6 +27,5 @@ class Runner:
         n.tf.printF()
 
         print("last")
-        print(Thomsons.match("(a-z)*.@.((a-z)|(0-9))*./.","af@fd4d3a."))
-        print(Thomsons.match("(a.b.c)+", ""))
-        print(Shunting.Converter().toPofix("a|(((a.b)*.(a|b)).a*)"))
+        print(Thomsons.match("a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a?.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a.a","aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"))
+        print(Thomsons.match("a?", "aa"))
--- src/gmit/re/com/Thomsons.py
+++ src/gmit/re/com/Thomsons.py
@@ -1,10 +1,22 @@
+# Thomson's construction base on class video
+# Jose Retamal
+# Graph theory project GMIT 2019
+
+# Base on:
+# https://swtch.com/~rsc/regexp/regexp1.html
+# https://web.microsoftstream.com/video/946a7826-e536-4295-b050-857975162e6c
+# https://web.microsoftstream.com/video/5e2a482a-b1c9-48a3-b183-19eb8362abc9
+
 from src.gmit.re.com import Shunting
 
-# Thomson's construction from class video
 
 # Represent a state with two arrosws labelled by label
 # use None for label representing "e" arrows
 class state:
+    """
+    Represent a state with 2 transitions
+    Non label represent the "E" arrow
+    """
     label = None
     edge1 = None
     edge2 = None
@@ -12,23 +24,39 @@ class state:
 
 # An NFA is represented by its initial and accept state
 class NFA:
+    """
+    NFA with inital and accpet state
+    """
     intial = None
     accept = None
 
     def __init__(self, initial, accept):
+        """
+        Create NFA
+        :param initial: Points to initial state
+        :param accept:  Points to accept state
+        """
         self.initial = initial
         self.accept = accept
 
 
 def compile(pofix):
+    """
+    Compile a postfix expression into a NFA
+    :param pofix: Postfix expression to compile.
+    :return: The new created NFA.
+    """
 
+    # Used for create the NFA using Thomson's constructions,
+    # will have a single NFA at the end.
     nfastack = []
 
+    # For escape character.
     isEscape = False;
 
     for c in pofix:
         if isEscape:
-            # If is escape, add NFA to stack/
+            # If is escape, add new simple NFA to stack.
             # Create new initial and accept state
             accept = state()
             initial = state()
@@ -38,13 +66,15 @@ def compile(pofix):
             # Push new NFA to the stack
             nfastack.append(NFA(initial, accept))
 
-            isEscape=False
+            # Next character will be not escaped.
+            isEscape = False
         else:
             if c == '/':
                 # Escape for next character
-                isEscape=True
+                isEscape = True
 
             elif c == '.':
+                # Pop NFA 1 and 2 from stack
                 nfa2 = nfastack.pop()
                 nfa1 = nfastack.pop()
 
@@ -55,10 +85,11 @@ def compile(pofix):
                 nfastack.append(NFA(nfa1.initial, nfa2.accept))
 
             elif c == '|':
+                # Pop NFA 1 and 2 from stack
                 nfa2 = nfastack.pop()
                 nfa1 = nfastack.pop()
 
-                # new states
+                # Create new initial and accept states
                 initial = state()
                 accept = state()
 
@@ -72,6 +103,7 @@ def compile(pofix):
 
                 # push nfa to the stack
                 nfastack.append(NFA(initial, accept))
+
             elif c == '*':
                 # Pop single NFA for stack
                 nfa1 = nfastack.pop()
@@ -87,64 +119,78 @@ def compile(pofix):
 
                 # Push the new NFA to the stac
                 nfastack.append(NFA(initial, accept))
-            elif c == "+":
 
+            elif c == "+":
                 # Pop single NFA for stack
                 nfa1 = nfastack.pop()
                 # Create new initial and accept state
                 initial = state()
                 accept = state()
-                # join new initial state to nfa1's initial state and the new accept state
+                # Join new initial state to nfa1's initial state
                 initial.edge1 = nfa1.initial
 
-                # joint the old accept state to the new accept stte and nfa1's initial statte
+                # Join the old accept state to the new accept state and nfa1's initial state.
                 nfa1.accept.edge1 = nfa1.initial
                 nfa1.accept.edge2 = accept
 
                 # Push the new NFA to the stac
                 nfastack.append(NFA(initial, accept))
+
             elif c == "?":
                 # Pop single NFA for stack
                 nfa1 = nfastack.pop()
+                # Create new initial and accept state
                 initial = state()
                 accept = state()
+
+                # Single state for accept zero occurrence.
                 non = state()
+                # New inital points to nfa1 initial(old initial)
+                # and to the new state created.
                 initial.edge1 = nfa1.initial
                 initial.edge2 = non
+                # nfa1 points to new accept state so does
+                # new created state.
                 nfa1.accept.edge1 = accept
                 non.edge1 = accept
-                nfastack.append(NFA(initial,accept))
+                # Push the  created NFA to stack
+                nfastack.append(NFA(initial, accept))
+
             elif c == "-":
+                # Pop 2 most recent.
                 nfa2 = nfastack.pop()
+                # We will grow this nfa
                 nfa1 = nfastack.pop()
+
+                # Get symbol of the nfa1 and 2,
+                # this two are the boundaries of the range : c1-c2.
                 c1 = nfa1.initial.label
-
-                c2 =  nfa2.initial.label
-
+                c2 = nfa2.initial.label
 
                 nfa = None;
 
-                for x in range(ord(c1)+1,ord(c2)):
+                for x in range(ord(c1) + 1, ord(c2)):
+                    # Loop through each character in the range of c1-c2,
+                    # creating a union between them: c1|c1.1|c1.2|...
 
+                    # create a new NFA that accept x
                     accept0 = state()
                     initial0 = state()
                     initial0.label = chr(x)
                     initial0.edge1 = accept0
+                    nfa0 = NFA(initial0, accept0)
 
-                    nfa0 = NFA(initial0,accept0)
-
+                    # Union of the new created nfa to the growing nfa.
                     initial2 = state()
                     accept2 = state()
-
                     initial2.edge1 = nfa1.initial
                     initial2.edge2 = nfa0.initial
-
                     nfa1.accept.edge1 = accept2
                     nfa0.accept.edge1 = accept2
+                    nfa1 = NFA(initial2, accept2)
 
-                    nfa1 = NFA(initial2,accept2)
-
-
+                # We have only do c1-c(2-1)
+                # So we do union of nfa2 to the growing NFA
                 initial = state()
                 accept = state()
                 # join new initial state to nfa1's initial state and the new accept state
@@ -152,12 +198,10 @@ def compile(pofix):
                 initial.edge2 = nfa2.initial
                 # joint the old accept state to the new accept stte and nfa1's initial statte
                 nfa1.accept.edge1 = accept
-                nfa2.accept.edge1= accept
+                nfa2.accept.edge1 = accept
 
-                # Push the new NFA to the stac
-                #nfastack.append(NFA(initial, accept))
-
-                nfastack.append(NFA(initial,accept))
+                # Push new NFA to stack
+                nfastack.append(NFA(initial, accept))
 
             else:
                 # Create new initial and accept state
@@ -170,11 +214,15 @@ def compile(pofix):
                 nfastack.append(NFA(initial, accept))
 
     # nfa stack should have only one single element
-
     return nfastack.pop()
 
+
 def followes(state):
-    """Return the set of states that can be reached from state following e arrows and his state."""
+    """
+    Return the set of states that can be reached from state following e arrows and his state.
+    :param state: starting state from where e arrows will follow
+    :return: The set of states that can be reached from param state following e arrows.
+    """
 
     # Create a new set with state as it's only member.
     states = set()
@@ -195,8 +243,13 @@ def followes(state):
     return states
 
 
-def match(infix,string):
-    """Matches string to the infix regular expression."""
+def match(infix, string):
+    """
+    Matches string to the infix regular expression.
+    :param infix: Infix regex.
+    :param string: String for match the regex.
+    :return: True if the regex match on the string.
+    """
 
     # shunt and compile the regular expresion
     postfix = Shunting.Converter().toPofix(infix);
@@ -222,7 +275,4 @@ def match(infix,string):
         next = set()
 
     # Check if the accept stare is in the set of current states.
-    return(nfa.accept in current)
-
-
-
+    return (nfa.accept in current)
